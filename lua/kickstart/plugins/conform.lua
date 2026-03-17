7c258a67b5e640d83a0c10fe3553b5c778714f8e
wIGt1M <wigt1mdev@gmail.com>
2026-03-17 12:09:06 +0100

    add python lsp plus linting and typechecking

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -31,6 +31,7 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        python = { 'black' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -223,7 +223,17 @@ return {
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
-        --
+        basedpyright = {
+          filetypes = { 'python' },
+          settings = {
+            basedpyright = {
+              analysis = {
+                typeCechkingMode = 'recommended',
+              },
+            },
+          },
+        },
+        ruff = {},
 
         lua_ls = {
           -- cmd = { ... },
@@ -257,6 +267,10 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'mypy', --
+        'ruff',
+        'basedpyright',
+        'black',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
